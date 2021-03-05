7b61de44701acc220888cb021d6e9337039559f3
lhoney614 <alicewhite00@mail.ru>
2021-03-05 13:53:02 +0700

    В пользовательском интерфейсе реализовано создание экземпляров классов логики

--- NoteApp/NoteAppUI/MainForm.cs
+++ NoteApp/NoteAppUI/MainForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using NoteApp;
+using Enum = System.Enum;
 
 namespace NoteAppUI
 {
@@ -15,8 +16,25 @@ namespace NoteAppUI
     {
         public MainForm()
         {
-            //check
             InitializeComponent();
+            comboBox1.DataSource = Enum.GetValues(typeof(NoteCategory));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Note note = new Note(DateTime.Now);
+
+            note.Title = textBox1.Text;
+            note.Text = textBox2.Text;
+            note.IsChanged = DateTime.Now;
+
+            string defaultPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/NoteApp/";
+
+            Project serialize = new Project {Notes = {note}};
+            ProjectManager.SaveToFile(serialize, defaultPath);
+
+            Project deserialize = ProjectManager.LoadFromFile(defaultPath);
+
         }
     }
 }
