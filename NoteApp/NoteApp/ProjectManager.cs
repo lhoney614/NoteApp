aa7754de89f1bb8331c416d64e30e819f67e7ddb
lhoney614 <alicewhite00@mail.ru>
2021-03-05 13:35:13 +0700

    Реализован метод сохранения и загрузки файла

--- NoteApp/NoteApp/ProjectManager.cs
+++ NoteApp/NoteApp/ProjectManager.cs
@@ -1,7 +1,51 @@
-﻿namespace NoteApp
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace NoteApp
 {
     public class ProjectManager
     {
-        
+        /// <summary>
+        /// Название файла для сохранений и загрузки
+        /// </summary>
+        private const string FileName = "NoteApp.notes";
+
+        public static void SaveToFile(Project project, string path)
+        {
+            Directory.CreateDirectory(path);
+            path += FileName;
+            JsonSerializer serializer = new JsonSerializer();
+
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                using (JsonTextWriter textWriter = new JsonTextWriter(writer))
+                {
+                    serializer.Serialize(textWriter, project);
+                }
+            }
+        }
+
+        public static Project LoadFromFile(string path)
+        {
+            path += FileName;
+
+            Project project;
+            JsonSerializer serializer = new JsonSerializer();
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                using (JsonTextReader textReader = new JsonTextReader(reader))
+                {
+                    project = (Project) serializer.Deserialize<Project>(textReader);
+                    if (project == null)
+                    {
+                        project = new Project();
+                    }
+
+                    return project;
+
+                }
+            }
+        }
     }
 }
