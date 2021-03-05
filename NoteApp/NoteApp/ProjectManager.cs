21bd4b452942849fcbeb9225b678df6f78f80f8a
lhoney614 <alicewhite00@mail.ru>
2021-03-05 21:13:31 +0700

    Добавление новых комментариев в класс ProjectManager

--- NoteApp/NoteApp/ProjectManager.cs
+++ NoteApp/NoteApp/ProjectManager.cs
@@ -10,32 +10,55 @@ namespace NoteApp
         /// </summary>
         private const string FileName = "NoteApp.notes";
 
+        /// <summary>
+        /// Сериализация (сохранение файла)
+        /// </summary>
+        /// <param name="project"></param>
+        /// <param name="filename">Название файла</param>
         public static void SaveToFile(Project project, string filename)
         {
+            //Создает каталог по указанному пути, если он не существует
             Directory.CreateDirectory(filename);
             filename += FileName;
+
+            //Создается экземпляр сериализатора
             JsonSerializer serializer = new JsonSerializer();
 
+            //Открывается поток для записи в файл с указанием пути
             using (StreamWriter writer = new StreamWriter(filename))
             {
                 using (JsonTextWriter textWriter = new JsonTextWriter(writer))
                 {
+                    //Вызывается сериализация и передается файл, 
+                    //который нужно сериализовать
                     serializer.Serialize(textWriter, project);
                 }
             }
         }
 
+        /// <summary>
+        /// Десериализация (загрузка файла)
+        /// </summary>
+        /// <param name="filename">Название файла</param>
+        /// <returns></returns>
         public static Project LoadFromFile(string filename)
         {
             filename += FileName;
 
-            Project project;
+            //Создается переменная, которая будет хранить
+            //результат десериализации
+            Project project = null;
+
+            //Создается экземпляр сериализатора
             JsonSerializer serializer = new JsonSerializer();
 
+            //Открывается поток для чтения из файла с указанием пути
             using (StreamReader reader = new StreamReader(filename))
             {
                 using (JsonTextReader textReader = new JsonTextReader(reader))
                 {
+                    //Вызывается десериализация и явно
+                    //преобразуется результат в целевой тип данных
                     project = (Project) serializer.Deserialize<Project>(textReader);
 
                     if (project == null)
